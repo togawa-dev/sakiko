c9c9e06930be86df5215b79e6e2db2e5d4685b3b
Machinacanis <hf4minecraft@163.com>
2025-12-26 05:40:36 +0800

    修复无法正常替换版本信息的bug，推送自动构建脚本

--- packages/core/uika/src/global.ts
+++ packages/core/uika/src/global.ts
@@ -1,2 +1,5 @@
-export const VERSION = "__VERSION__";
-export const PKG_NAME = "__PKG_NAME__";
+declare const __VERSION__: string;
+declare const __PKG_NAME__: string;
+
+export const VERSION = __VERSION__;
+export const PKG_NAME = __PKG_NAME__;
